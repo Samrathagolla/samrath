296b3300e0265b43664316c4c7190abe053a435f
Samrathagolla <99330181+Samrathagolla@users.noreply.github.com>
2022-02-15 21:57:06 +0530

    Create Sam.py

---
+++ Sam.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    add=a+b
+    print(add)
+    subtract=a-b
+    print(subtract)
+    multiply=a*b
+    print(multiply)
+Input (stdin)
+
+
+3
+2
+Expected Output
+
+5
+1
+6
