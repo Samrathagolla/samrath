f1625b47cf02d46991edc73446c79c921f55f7db
Samrathagolla <99330181+Samrathagolla@users.noreply.github.com>
2022-02-15 21:59:04 +0530

    Create Samra.py

---
+++ Samra.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+
+division=a//b
+print(division)
+floatdivision=a/b
+print(floatdivision)
+Input (stdin)
+
+4
+3
+Your Output (stdout)
+1
+1.33333333333
